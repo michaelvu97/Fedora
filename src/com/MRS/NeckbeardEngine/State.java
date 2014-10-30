558969ec1aa485608fff27ca036894bfc04510c3
Michael Vu <michael.vu.97@outlook.com>
2014-10-30 12:25:14 -0400

    Documentation Changes

--- src/com/MRS/NeckbeardEngine/State.java
+++ src/com/MRS/NeckbeardEngine/State.java
@@ -1,4 +1,15 @@
+/* 
+ * PROJECT:LodeStar
+ * Source can be found at www.github.com/michaelvu97/LodeStar
+ * Authors: Qazi Qazi (Project Manager), Roy Liu, Michael Vu
+ * Inception Date: 9/17/14
+ *
+ * This is the enumeration for the 3 possible states for objects in the game:
+ * RED, BLUE, or BOTH
+ */
+ 
 package com.MRS.NeckbeardEngine;
+
 public enum State {
 	RED (1),
 	BLUE (2),
