b1798a036f099d60c000c5a8aa4a42a47bb171fa
Michael Vu <michael.vu.97@outlook.com>
2014-10-31 13:06:37 -0400

    Formatting, added abstract methods

--- src/com/MRS/NeckbeardEngine/Projectile.java
+++ src/com/MRS/NeckbeardEngine/Projectile.java
@@ -1,21 +1,48 @@
-abstract class Projectile{
+/*
+ * PROJECT: LodeStar
+ * Source code can be found at www.github.com/michaelvu97/LodeStar
+ * Authors: Safwan Qazi (Project Manager), Michael Vu, Roy Liu
+ * Inception Date: 9/17/14
+ *
+ * The abstraction for any projectiles in game.
+ */
+
+package com.MRS.NeckbeardEngine;
+
+abstract class Projectile {
+     
+     //Fields
      private State state;
      private int x;
      private int y;
      private int xVelocity;
      private int yVelocity;
+     
      private String imgPath;
      private HitBox hitBox;
+     
+     //Static fields
      public static int playerShotVelocity = 1;
      public static int playerScatterShotXVelocity = 1;
      public static int playerScatterShotYVelocity = 1;
      public static int playerFastShotVelocity = 2;
      
-     public Projectile(int x,int y,State state,String imgPath){
-          this.x = x;
-          this.y = y;
-          this.state = state;
-          this.imgPath = imgPath;
-     }
+     //Accessors
+     abstract int getState ();
+     abstract int getX ();
+     abstract int getY ();
+     abstract int getXVelocity ();
+     abstract int getYVelocity ();
+     abstract String getImgPath ();
+     abstract HitBox getHitBox ();
+     
+     //Mutators
+     abstract void setState (int state);
+     abstract void setX (int x);
+     abstract void setY (int y);
+     abstract void setXVelocity (int xVelocity);
+     abstract void setYVelocity (int yVelocity);
+     abstract void setImgPath (String imgPath);
+     abstract void setHitBox (HitBox hitBox);
 }
 
