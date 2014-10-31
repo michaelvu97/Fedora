b6a93711bb9a87a3a781f1bd0ad34303e065f39a
PeteDophile <PeteDophile@users.noreply.github.com>
2014-10-31 13:03:56 -0400

    Update HitScan.java

--- src/com/MRS/NeckbeardEngine/HitScan.java
+++ src/com/MRS/NeckbeardEngine/HitScan.java
@@ -3,14 +3,6 @@ abstract class HitScan extends HitBox {
      private State state;
      private double duration;
      private long killTime;
-     
-     public HitScan(int x,int y,State state,double duration,int height,int width) {
-          this.state = state;
-          this.duration = duration;
-          super(x,y,width,height);
-          killTime = duration+System.currentTimeMillis();
-     }
-     
      public int getState();
      public double getDuration();
      public long getkillTime();
