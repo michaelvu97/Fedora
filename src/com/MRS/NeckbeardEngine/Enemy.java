7e89edb1a6118fa516c2fb28884bd2d0a50ac1c0
PeteDophile <PeteDophile@users.noreply.github.com>
2014-10-31 13:09:33 -0400

    Deleted constructor. Updated timeLine from int to long. Added accessors.

--- src/com/MRS/NeckbeardEngine/Enemy.java
+++ src/com/MRS/NeckbeardEngine/Enemy.java
@@ -1,5 +1,5 @@
 *PROJECT: LodeStar
- * Source can be fooun at www.github.com/michaelvu97/LodeStar
+ * Source can be found at www.github.com/michaelvu97/LodeStar
  * Authors: Safwan Qazi(Project Manager), Roy Liu, Michael Vu
  * Date:10/30/2014
  * 
@@ -15,9 +15,21 @@ abstract class Enemy {
   private int xVelocity;
   private int yVelocity;
   private Projectile  projectileType;//will be more specific for every enemy
-  private PowerUpPickup helpPowerUp;//if they have power up it is a type if not it is null
-  private int timeLine;//used to determine at what time they will enter the game screen/spawn
+  private PowerUpPickup heldPowerUp;//if they have power up it is a type if not it is null
+  private long timeLine;//used to determine at what time they will enter the game screen/spawn
   
-  abstract public Enemy(int x int y, State state, int xVelocity, int yVelocity, PowerUpPickup heldPowerUp);//only asks for fields which are variable between every instance
+  //Accessors
+  public int getState();
+  public int getHealth();
+  public int getX();
+  public int getY();
+  public int getYVelocity();
+  public int getXVelocity();
+  public Projectile projectileType();
+  public PowerUpPickup heldPowerUp();
+  public long timeLine();
+  
   abstract public void animate();//to animate the enemy
+  
+  
 }
