aa2aebb65d1342d5e12d7fe47f4b92b18c02c578
Michael Vu <michael.vu.97@outlook.com>
2014-11-03 13:43:44 -0500

    Create ImagePaths.java

---
+++ src/com/MRS/NeckbeardEngine/ImagePaths.java
@@ -0,0 +1,8 @@
+/*
+ * This class is just for the static image paths, not for instantiation
+ */
+package com.MRS.NeckbeardEngine;
+
+public class ImagePaths {
+     public final String playerPath = "Assets//TestImage.png";
+}
