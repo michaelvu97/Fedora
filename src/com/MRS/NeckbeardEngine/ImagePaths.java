ae390b9cd281d9ebfd462aa17ce543e160884a18
Michael Vu <michael.vu.97@outlook.com>
2014-11-03 14:00:59 -0500

    Delete ImagePaths.java

--- src/com/MRS/NeckbeardEngine/ImagePaths.java
+++
@@ -1,8 +0,0 @@
-/*
- * This class is just for the static image paths, not for instantiation
- */
-package com.MRS.NeckbeardEngine;
-
-public class ImagePaths {
-     public final String playerPath = "Assets//TestImage.png";
-}
