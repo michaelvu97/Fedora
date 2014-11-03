729c1c9727cab225ba7e70710529de55b20c565e
Michael Vu <michael.vu.97@outlook.com>
2014-11-03 14:01:46 -0500

    Create FileStore.java

---
+++ src/com/MRS/NeckbeardEngine/FileStore.java
@@ -0,0 +1,8 @@
+/*
+ * This class is just for the static image paths, not for instantiation
+ */
+package com.MRS.NeckbeardEngine;
+
+public class FileStore {
+     public final static String PLAYER_TEST = "Assets//TestImage.png";
+}
