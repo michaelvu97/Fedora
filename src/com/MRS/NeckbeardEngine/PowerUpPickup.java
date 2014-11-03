c5def0a058e0550588da7e5e47c1a4ebffb4cb5a
Michael Vu <michael.vu.97@outlook.com>
2014-11-03 13:47:10 -0500

    Typos

--- src/com/MRS/NeckbeardEngine/PowerUpPickup.java
+++ src/com/MRS/NeckbeardEngine/PowerUpPickup.java
@@ -1,15 +1,17 @@
 package com.MRS.NeckbeardEngine;
+
 class PowerUpPickup {
   private int x;
   private int y;
   private int xVelocity;
   private int yVelocity;
   private PowerUp heldPowerUp;
+  
   public PowerUpPickup(int x, int y, int xVelocity, int yVelocity, PowerUp heldPowerUp) {
     this.x = x;
     this.y = y;
     this.xVelocity = xVelocity;
     this.yVelocity = yVelocity;
-    this.heldPowerup = heldPowerup;
+    this.heldPowerUp = heldPowerUp;
   }
 }
