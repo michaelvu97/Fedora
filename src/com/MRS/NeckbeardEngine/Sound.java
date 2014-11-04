45096b473b6d2cd491cbf1f41f0fa439b4bdf229
TheImportantOne <safster4@gmail.com>
2014-11-03 23:57:51 -0500

    Incomplete
    Currently has play and loop(forever and certain times)
    Still need to work on stopping while playing, and volume

---
+++ src/com/MRS/NeckbeardEngine/Sound.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.net.URL;
+import javax.sound.sampled.*;
+public class  Sound{
+public static Mixer mixer;
+public static Clip clip;
+
+	public Sound(String file) {
+		Mixer.Info[] mixInfos = AudioSystem.getMixerInfo();
+		mixer = AudioSystem.getMixer(mixInfos[0]);
+		DataLine.Info dataInfo = new DataLine.Info(Clip.class, null);
+		try{clip = (Clip)mixer.getLine(dataInfo);}
+		catch(LineUnavailableException lue) {lue.printStackTrace();}
+		try {
+			URL soundURL = Sound.class.getResource(file);
+			AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+			clip.open(audioStream);
+		}
+		catch(LineUnavailableException lue){lue.printStackTrace();}
+		catch(UnsupportedAudioFileException uafe){uafe.printStackTrace();}
+		catch(IOException ioe){ioe.printStackTrace();}
+	}
+		public void play()
+	{
+		clip.start();
+		do {
+		try{Thread.sleep(50);}
+		catch(InterruptedException ie){ie.printStackTrace();}
+		}while(clip.isActive());
+	}
+		public void loop() {
+			clip.loop(clip.LOOP_CONTINUOUSLY);
+			do {
+				try{Thread.sleep(50);}
+				catch(InterruptedException ie){ie.printStackTrace();}
+				}while(clip.isActive());
+		}
+		public void customLoop(int times){
+		clip.loop(times-1);
+		do {
+			try{Thread.sleep(50);}
+			catch(InterruptedException ie){ie.printStackTrace();}
+			}while(clip.isActive());
+		}
+		public void stop()
+		{
+			clip.stop();
+		}
+		
+		public void volume(float vol) {
+			FloatControl gainControl = 
+				    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+				gainControl.setValue(vol);
+		}
+}
