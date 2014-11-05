8827aafa4da3b04ef0e72aad0c7af14da6d8dbda
Michael Vu <michael.vu.97@outlook.com>
2014-11-05 14:12:01 -0500

    Fixed Safwan's terrible mistakes
    made it public

--- src/com/MRS/NeckbeardEngine/Enemy.java
+++ src/com/MRS/NeckbeardEngine/Enemy.java
@@ -7,47 +7,18 @@
  * This is the abstract which determines what fields and methods all enemies will have.
  */
 package com.MRS.NeckbeardEngine;
-abstract class Enemy {
-  private State state;//enums
-  private int health;
-  private int x;
-  private int y;
-  private int xVelocity;
-  private int yVelocity;
-  private Projectile  projectileType;//will be more specific for every enemy
-  private PowerUpPickup heldPowerUp;//if they have power up it is a type if not it is null
-  private long timeLine;//used to determine at what time they will enter the game screen/spawn
-  
-  //Accessors
-  public int getState() { 
-       return state.state();
-  }
-  public int getHealth() {
-       return health;
-  }
-  public int getX() {
-       return x;
-  }
-  public int getY() {
-       return y;
-  }
-  public int getYVelocity() {
-       return yVelocity;
-  }
-  public int getXVelocity() {
-       return xVelocity;
-  }
-  public Projectile projectileType() {
-       return projectileType;
-  }
-  public PowerUpPickup heldPowerUp() {
-       return heldPowerUp;
-  }
-  public long timeLine() {
-       return timeLine;
-  }
-  
-  abstract public void animate();//to animate the enemy
-  
-  
+public abstract class Enemy {
+     public State state;//enums
+     public int health;
+     public int x;
+     public int y;
+     public int xVelocity;
+     public int yVelocity;
+     public Projectile  projectileType;//will be more specific for every enemy
+     public PowerUpPickup heldPowerUp;//if they have power up it is a type if not it is null
+     public long timeLine;//used to determine at what time they will enter the game screen/spawn
+     
+     abstract public void animate();//to animate the enemy
+     
+     
 }
