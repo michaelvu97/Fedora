4ae7f9f16b13a87812dc22f2878f2a9fe5e2785b
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-10 13:02:54 -0500

    Create new file

---
+++ src/com/MRS/NeckbeardEngine/Enemies/Iris.java
@@ -0,0 +1,17 @@
+package com.MRS.NeckbeardEngine.Enemies;
+
+import com.MRS.NeckbeardEngine.*;
+
+public class Iris extends Enemy {
+     
+     public Iris (State state, int x, int y, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+          super(state, x, y, 0, yVelocity, projectileType, heldPowerUp, timeLine);
+          health = 2;
+     }
+     @Override
+     public void animate () {
+      
+          //Swag
+          
+     }
+}
