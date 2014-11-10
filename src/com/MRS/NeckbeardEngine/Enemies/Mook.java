798108b975af8a354f322a03f58ec672f7150a2e
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-10 12:28:15 -0500

    Update Mook.java

--- src/com/MRS/NeckbeardEngine/Enemies/Mook.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Mook.java
@@ -5,7 +5,7 @@ import com.MRS.NeckbeardEngine.*;
 public class Mook extends Enemy {
      
      public Mook (State state, int health, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, health, x, y, xVelociy, yVelocity, projectileType, heldPowerUp, timeLine);
+          super(state, health, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
      }
      @Override
      public void animate () {
