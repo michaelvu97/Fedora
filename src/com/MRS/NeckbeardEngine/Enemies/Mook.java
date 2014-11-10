eb095880735e657b85b06afe4625775ee5587bd7
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-10 12:19:24 -0500

    Added super constructor and fields from abstract Enemy

--- src/com/MRS/NeckbeardEngine/Enemies/Mook.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Mook.java
@@ -4,13 +4,8 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Mook extends Enemy {
      
-     public Mook (int x, int y, int xVelocity, int yVelocity, PowerUpPickup powerUpPickup) { //Health?
-          this.x = x;
-          this.y = y;
-          this.xVelocity = xVelocity;
-          this.yVelocity = yVelocity;
-          this.heldPowerUp = powerUpPickup;
-          //Shot type
+     public Mook (State state, int health, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+          super(state, health, x, y, xVelociy, yVelocity, projectileType, heldPowerUp, timeLine);
      }
      @Override
      public void animate () {
