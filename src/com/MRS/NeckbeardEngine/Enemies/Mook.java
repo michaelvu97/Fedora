a522a756c3a66617ec5f2ef5b01b0d412d538c81
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-10 12:36:09 -0500

    Manually set Mook Health

--- src/com/MRS/NeckbeardEngine/Enemies/Mook.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Mook.java
@@ -4,8 +4,9 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Mook extends Enemy {
      
-     public Mook (State state, int health, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, health, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Mook (State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+          health = 1;
      }
      @Override
      public void animate () {
