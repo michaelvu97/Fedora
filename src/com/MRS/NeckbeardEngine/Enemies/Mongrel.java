167b9ce0a818985bf8027f1aa47659b336a6879e
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-10 12:43:11 -0500

    Create new file

---
+++ src/com/MRS/NeckbeardEngine/Enemies/Mongrel.java
@@ -0,0 +1,17 @@
+package com.MRS.NeckbeardEngine.Enemies;
+
+import com.MRS.NeckbeardEngine.*;
+
+public class Mongrel extends Enemy {
+     
+     public Mongrel (State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+          health = 1;
+     }
+     @Override
+     public void animate () {
+      
+          //Swag
+          
+     }
+}
