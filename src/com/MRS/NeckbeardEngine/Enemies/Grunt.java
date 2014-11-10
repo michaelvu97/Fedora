80642778dbe3e85510bdb146a3c53282da8b2275
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-10 12:35:24 -0500

    Manually set Grunt Health

--- src/com/MRS/NeckbeardEngine/Enemies/Grunt.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Grunt.java
@@ -4,8 +4,9 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Grunt extends Enemy {
      
-     public Grunt (State state, int health, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, health, x, y, xVelociy, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Grunt (State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+          super(state, x, y, xVelociy, yVelocity, projectileType, heldPowerUp, timeLine);
+          health = 1;
      }
      @Override
      public void animate () {
