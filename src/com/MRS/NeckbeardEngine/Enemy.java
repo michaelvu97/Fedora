fe93621345c93b55a0eda6446a978fadf93f3cca
TheImportantOne <safster4@gmail.com>
2014-11-10 12:57:37 -0500

    fixed my stupid mistakes

--- src/com/MRS/NeckbeardEngine/Enemy.java
+++ src/com/MRS/NeckbeardEngine/Enemy.java
@@ -7,87 +7,82 @@
  * This is the abstract which determines what fields and methods all enemies will have.
  */
 package com.MRS.NeckbeardEngine;
-i
 public abstract class Enemy {
-     protected State state;//enums
-     protected int health;
-     protected int x;
-     protected int y;
-     protected int xVelocity;
-     protected int yVelocity;
-     protected Projectile  projectileType;//will be more specific for every enemy
-     protected PowerUpPickup heldPowerUp;//if they have power up it is a type if not it is null
-     protected long timeLine;//used to determine at what time they will enter the game screen/spawn
-     public Enemy(State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-      this.state = state;
-      this.x = x;
-      this.y = y;
-      this.xVelocity = xVelocity;
-      this.yVelocity = yVelocity;
-      this.projectileType = projectileType;
-      this.heldPowerUp = heldPowerUp;
-      this.timeLine = timeLine;
-     }
-
-    public State getState() {
-      return state;   
-    }
-    public void setState(State state) {
-      this.state = state;
-    }
-    public int getHealth() {
-      return health;   
-    }
-    public void setHealth(int health) {
-      this.health = health;
-    }
-    public int getX() {
-         return x;
-    }
-    public void setX(int x) {
-         this.x = x;
-    }
-    public int getY() {
-         return y;
-    }
-    public void setY(int y) {
-         this.y = y;
-    }
-    public int getXVelocity() {
-         return xVelocity;
-    }
-    public void setXVelocity(int xVelocity) {
-         this.xVelocity = xVelocity;
-    }
-    public int getYVelocity() {
-         return yVelocity;
-    }
-    public void setYVelocity(int yVelocity) {
-         this.yVelocity = yVelocity;
-    }
-    public Projectile getProjectileType() {
-         return projectileType;
-    }
-    public void setProjectileType(Projectile projectileType) {
-         this.projectileType = projectileType;
-    }
-    public PowerUpPickup getHeldPowerUp() {
-         return heldPowerUp;
-    }
-    public void setHeldPowerUp(PowerUpPickup heldPowerUp) {
-         this.heldPowerUp = heldPowerUp;
-    }
-    public long getTimeLine () {
-         return timeLine;
-    }
-    public void setTimeLine(long timeLine) {
-         this.timeLine = timeLine;
-    }
-    }
-    }
-     
-     abstract public void animate();//to animate the enemy
-     
-     
-}
+  protected State state;//enums
+  protected int health;
+  protected int x;
+  protected int y;
+  protected int xVelocity;
+  protected int yVelocity;
+  protected Projectile  projectileType;//will be more specific for every enemy
+  protected PowerUpPickup heldPowerUp;//if they have power up it is a type if not it is null
+  protected long timeLine;//used to determine at what time they will enter the game screen/spawn
+  public Enemy(State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+    this.state = state;
+    this.x = x;
+    this.y = y;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
+    this.projectileType = projectileType;
+    this.heldPowerUp = heldPowerUp;
+    this.timeLine = timeLine;
+  }
+  
+  public State getState() {
+    return state;   
+  }
+  public void setState(State state) {
+    this.state = state;
+  }
+  public int getHealth() {
+    return health;   
+  }
+  public void setHealth(int health) {
+    this.health = health;
+  }
+  public int getX() {
+    return x;
+  }
+  public void setX(int x) {
+    this.x = x;
+  }
+  public int getY() {
+    return y;
+  }
+  public void setY(int y) {
+    this.y = y;
+  }
+  public int getXVelocity() {
+    return xVelocity;
+  }
+  public void setXVelocity(int xVelocity) {
+    this.xVelocity = xVelocity;
+  }
+  public int getYVelocity() {
+    return yVelocity;
+  }
+  public void setYVelocity(int yVelocity) {
+    this.yVelocity = yVelocity;
+  }
+  public Projectile getProjectileType() {
+    return projectileType;
+  }
+  public void setProjectileType(Projectile projectileType) {
+    this.projectileType = projectileType;
+  }
+  public PowerUpPickup getHeldPowerUp() {
+    return heldPowerUp;
+  }
+  public void setHeldPowerUp(PowerUpPickup heldPowerUp) {
+    this.heldPowerUp = heldPowerUp;
+  }
+  public long getTimeLine () {
+    return timeLine;
+  }
+  public void setTimeLine(long timeLine) {
+    this.timeLine = timeLine;
+  }
+  
+  abstract public void animate();//to animate the enemy
+  
 }
