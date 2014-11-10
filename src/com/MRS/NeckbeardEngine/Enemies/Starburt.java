e65731702bf5b635336c1ec7e4919f0822a2981a
Michael Vu <michael.vu.97@outlook.com>
2014-11-10 12:56:33 -0500

    Update Starburt.java

--- src/com/MRS/NeckbeardEngine/Enemies/Starburt.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Starburt.java
@@ -1,1 +1,15 @@
 package com.MRS.NeckbeardEngine.Enemies;
+
+import com.MRS.NeckbeardEngine.*;
+
+public class Starburt extends Enemy {
+  
+  public Starburt (State state, int health, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickUp heldPowerUp, long timeLine) {
+    super (state, health, x, y, xVelocity, 0, projectileType, heldPowerup, timeLine);
+  }
+  
+  @Override
+  public void animate () {
+    //shout out to safwan's sick type
+  }
+}
