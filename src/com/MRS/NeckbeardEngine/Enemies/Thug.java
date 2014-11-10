da5df62458561e9000f5185bfc1e3ced760666cd
Michael Vu <michael.vu.97@outlook.com>
2014-11-10 12:22:56 -0500

    Update Thug.java

--- src/com/MRS/NeckbeardEngine/Enemies/Thug.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Thug.java
@@ -2,13 +2,14 @@ package com.MRS.NeckbeardEngine.Enemies;
 
 public class Thug extends Enemy {
   
-   public Thug (int x, int y, int xVelocity, int yVelocity, PowerUpPickup powerUpPickup) {
+   public Thug (int x, int y, int xVelocity, int yVelocity, PowerUpPickup powerUpPickup, State state) {
 
     this.x = x;
     this.y = y;
     this.xVelocity = xVelocity;
     this.yVelocity = yVelocity;
     this.heldPowerUp = powerUpPickup;
+    this.state = state;
   }
   
   @Override
