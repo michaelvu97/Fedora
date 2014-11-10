03ec53a5e724ba54e03f572aa54e5e9873081b4e
Michael Vu <michael.vu.97@outlook.com>
2014-11-10 12:17:02 -0500

    Thug life

--- src/com/MRS/NeckbeardEngine/Enemies/Thug.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Thug.java
@@ -1,1 +1,18 @@
+package com.MRS.NeckbeardEngine.Enemies;
 
+public class Thug extends Enemy {
+  
+   public Thug (int x, int y, int xVelocity, int yVelocity, PowerUpPickup powerUpPickup) {
+
+    this.x = x;
+    this.y = y;
+    this.xVelocity = xVelocity;
+    this.yVelocity = yVelocity;
+    this.heldPowerUp = powerUpPickup;
+  }
+  
+  @Override
+  public void animate () {
+    //thug lyfe
+  }
+}
