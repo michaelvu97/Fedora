a280525cc55e596d83650c87e40e0abcadd2e655
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 12:20:28 -0500

    Change velocity to double

--- src/com/MRS/NeckbeardEngine/Enemies/Mongrel.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Mongrel.java
@@ -4,7 +4,7 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Mongrel extends Enemy {
      
-     public Mongrel (State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+     public Mongrel (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
           health = 1;
      }
