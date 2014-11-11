a8d3ac619cecfda38ad8eb32ce6dea2a95a64b01
TheImportantOne <safster4@gmail.com>
2014-11-11 13:15:54 -0500

    Update Shot.java

--- src/com/MRS/NeckbeardEngine/Projectile/Shot.java
+++ src/com/MRS/NeckbeardEngine/Projectile/Shot.java
@@ -1,1 +1,6 @@
-
+public class Shot extends Projectile {
+  public Shot (State state, int x, int y, double xVelocity, double yVelocity, String imgPath) {
+    super(state, x, y, xVelocity, yVelocity, imgPath);
+    
+  }
+}
