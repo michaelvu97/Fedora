94779a5f9b8e610f7a3ade4b14af23465b5ff802
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 17:59:54 -0500

    Added canShoot boolean field

--- src/com/MRS/NeckbeardEngine/Enemies/Elite.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Elite.java
@@ -8,8 +8,8 @@ public class Elite extends Enemy {
      public static int DEFAULT_HITBOX_WIDTH = 30;
      public static int DEFAULT_HTIBOX_HEIGHT = 20;
      
-     public Elite (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Elite (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
           health = 3;
           hitbox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT;
      }
