b7acc18651a62b91fdfa02380d02997d64c0685b
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 18:03:32 -0500

    Added canShoot boolean field

--- src/com/MRS/NeckbeardEngine/Enemies/Iris.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Iris.java
@@ -8,8 +8,8 @@ public class Iris extends Enemy {
      public static int DEFAULT_HITBOX_WIDTH = 20;
      public static int DEFAULT_HITBOX_HEIGHT = 20;
      
-     public Iris (State state, int x, int y, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, x, y, 0, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Iris (State state, int x, int y, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+          super(state, x, y, 0, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
           health = 2;
           hitBox = new HitBox (x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
      }
