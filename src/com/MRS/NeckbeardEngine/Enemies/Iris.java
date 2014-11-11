cc65de6cf6e48bee86fda38ef6cb7db81b8dee03
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 12:18:35 -0500

    Change velocity to double

--- src/com/MRS/NeckbeardEngine/Enemies/Iris.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Iris.java
@@ -4,7 +4,7 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Iris extends Enemy {
      
-     public Iris (State state, int x, int y, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+     public Iris (State state, int x, int y, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
           super(state, x, y, 0, yVelocity, projectileType, heldPowerUp, timeLine);
           health = 2;
      }
