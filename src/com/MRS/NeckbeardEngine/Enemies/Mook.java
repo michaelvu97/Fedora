660775dbc4eb2f0a03e73123cbf27832ca0b1911
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 12:28:29 -0500

    Added hitboxes and imported new projectile package

--- src/com/MRS/NeckbeardEngine/Enemies/Mook.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Mook.java
@@ -1,12 +1,17 @@
 package com.MRS.NeckbeardEngine.Enemies;
 
 import com.MRS.NeckbeardEngine.*;
+import com.MRS.NeckbeardEngine.Projectiles.*;
 
 public class Mook extends Enemy {
+     
+     public static int DEFAULT_HITBOX_WIDTH = 5;
+     public static int DEFAULT_HITBOX_HEIGHT = 5;
      
      public Mook (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
           health = 1;
+          hitBox = new HitBox (x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
      }
      @Override
      public void animate () {
