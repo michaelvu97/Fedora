8b7572f002a353ce00e83ffb7a585925fbb57d63
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 18:02:58 -0500

    Added canShoot boolean field

--- src/com/MRS/NeckbeardEngine/Enemies/Grunt.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Grunt.java
@@ -8,8 +8,8 @@ public class Grunt extends Enemy {
      public static int DEFAULT_HITBOX_WIDTH = 10;
      public static int DEFAULT_HITBOX_HEIGHT = 10;
      
-     public Grunt (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Grunt (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
           health = 1;
           hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
      }
