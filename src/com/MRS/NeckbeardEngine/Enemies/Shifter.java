f22ef1b4b9c5684c763681fb84158f87875fd2f5
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 18:04:50 -0500

    Added canShoot boolean field

--- src/com/MRS/NeckbeardEngine/Enemies/Shifter.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Shifter.java
@@ -4,8 +4,8 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Shifter extends Enemy {
      
-     public Shifter (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Shifter (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
           health = 1;
      }
      @Override
