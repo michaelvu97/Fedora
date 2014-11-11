3d89f1a140281d394b71b6a86245c783bd78b576
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 12:17:28 -0500

    Change velocity to double

--- src/com/MRS/NeckbeardEngine/Enemies/Elite.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Elite.java
@@ -4,7 +4,7 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Elite extends Enemy {
      
-     public Elite (State state, int x, int y, int xVelocity, int yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
+     public Elite (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
           health = 3;
      }
