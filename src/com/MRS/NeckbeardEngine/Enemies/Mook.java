25ab1aeae8c3b2ce9a5b7a06b39f42696fd7567b
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 18:04:26 -0500

    Added canShoot boolean field

--- src/com/MRS/NeckbeardEngine/Enemies/Mook.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Mook.java
@@ -8,8 +8,8 @@ public class Mook extends Enemy {
      public static int DEFAULT_HITBOX_WIDTH = 5;
      public static int DEFAULT_HITBOX_HEIGHT = 5;
      
-     public Mook (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Mook (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
           health = 1;
           hitBox = new HitBox (x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
      }
