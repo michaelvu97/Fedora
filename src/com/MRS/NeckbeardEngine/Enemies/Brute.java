e99a5baf56fa0263c07a79b86d42cb042fb567e0
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 12:53:37 -0500

    Added hitboxes and imported new projectile package

--- src/com/MRS/NeckbeardEngine/Enemies/Brute.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Brute.java
@@ -1,8 +1,12 @@
 package com.MRS.NeckbeardEngine.Enemies;
 
 import com.MRS.NeckbeardEngine.*;
+import com.MRS.NeckbeardEngine.Projectiles.*;
 
 public class Brute extends Enemy {
+     
+     public static int DEFAULT_HITBOX_WIDTH = 10;
+     public static int DEFUALT_HITBOX_HEIGHT = 10;
      
      public Brute (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
