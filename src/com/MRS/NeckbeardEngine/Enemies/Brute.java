676c2a6d9205ad3864ad843ffb57dc29abb345f0
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 17:59:27 -0500

    Added canShoot boolean field

--- src/com/MRS/NeckbeardEngine/Enemies/Brute.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Brute.java
@@ -8,8 +8,8 @@ public class Brute extends Enemy {
      public static int DEFAULT_HITBOX_WIDTH = 10;
      public static int DEFUALT_HITBOX_HEIGHT = 10;
      
-     public Brute (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
+     public Brute (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+          super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
           health = 3;
      }
      @Override
