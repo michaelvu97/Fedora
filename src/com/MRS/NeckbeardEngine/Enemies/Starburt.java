c8f4ae05335f1bacc9db37faf9499b8a85213582
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 18:05:23 -0500

    Added canShoot boolean field (Michael why you still garbage)

--- src/com/MRS/NeckbeardEngine/Enemies/Starburt.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Starburt.java
@@ -4,8 +4,8 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Starburt extends Enemy {
   
-  public Starburt (State state, int x, int y, double xVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
-    super (state, x, y, xVelocity, 0, projectileType, heldPowerUp, timeLine);
+  public Starburt (State state, int x, int y, double xVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
+    super (state, x, y, xVelocity, 0, projectileType, heldPowerUp, timeLine, canShoot);
     health = 2;
   }
   
