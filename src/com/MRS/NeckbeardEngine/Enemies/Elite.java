707612fee174298871c5d28a365d457253602d88
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 12:57:22 -0500

    Added hitboxes and imported new projectile package

--- src/com/MRS/NeckbeardEngine/Enemies/Elite.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Elite.java
@@ -4,9 +4,13 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Elite extends Enemy {
      
+     public static int DEFAULT_HITBOX_WIDTH = 30;
+     public static int DEFAULT_HTIBOX_HEIGHT = 20;
+     
      public Elite (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine);
           health = 3;
+          hitbox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT;
      }
      @Override
      public void animate () {
