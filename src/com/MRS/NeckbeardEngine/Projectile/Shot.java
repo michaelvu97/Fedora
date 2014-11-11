19c57886b65fa2e7adf9c7e3bb91532a1d6932db
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-11 17:55:23 -0500

    Added hitbox and duration to constructor

--- src/com/MRS/NeckbeardEngine/Projectile/Shot.java
+++ src/com/MRS/NeckbeardEngine/Projectile/Shot.java
@@ -1,6 +1,6 @@
 public class Shot extends Projectile {
-  public Shot (State state, int x, int y, double xVelocity, double yVelocity, String imgPath) {
-    super(state, x, y, xVelocity, yVelocity, imgPath);
+  public Shot (State state, int x, int y, double xVelocity, double yVelocity, String imgPath, Hitbox hitBox, double duration) {
+    super(state, x, y, xVelocity, yVelocity, imgPath, hitBox, duration);
     
   }
 }
