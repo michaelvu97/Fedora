095719c76ae964c563be4411fa8ea4c89d4507e0
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-12 13:15:25 -0500

    Update Goliath.java

--- src/com/MRS/NeckbeardEngine/Enemies/Bosses/Goliath.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Bosses/Goliath.java
@@ -5,4 +5,16 @@ import com.MRS.NeckbeardEngine.Projectile;
 import com.MRS.NeckbeardEngine.State;
 
 public class Goliath {
-
+  private State state;
+  private int health;
+  private int x;
+  private int y;
+  private Projectile projectileType;
+  private long timeLine;
+  private boolean canShoot;
+  
+  public static int DEFAULT_HITBOX_WIDTH = 10;
+  public static int DEFAULT_HITBOX_LENGTH = 10;
+  public Goliath () {
+    
+  }
