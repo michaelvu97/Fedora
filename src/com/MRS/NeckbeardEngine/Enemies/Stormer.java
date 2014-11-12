a21ea5211d7fb42792048b8fa038324baee46898
TheImportantOne <safster4@gmail.com>
2014-11-11 21:32:30 -0500

    Fixing Dumb Git

--- src/com/MRS/NeckbeardEngine/Enemies/Stormer.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Stormer.java
@@ -8,19 +8,11 @@ import com.MRS.NeckbeardEngine.State;
 
 public class Stormer extends Enemy {
 	
-	public static int DEFAULT_HITBOX_WIDTH = 5;
-    public static int DEFAULT_HITBOX_HEIGHT = 5;
-    
-     
-     public static int DEFAULT_HITBOX_WIDTH = 10;
-     public static int DEFAULT_HITBOX_HEIGHT = 10;
-     
      public static int DEFAULT_HITBOX_WIDTH = 10;
      public static int DEFAULT_HITBOX_HEIGHT = 10;
      
      public Stormer (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
-          hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
           health = 1;
           hitBox = new HitBox (x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
      }
