9d2366c9a4252e30812647506d33bde9a0f48618
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 19:45:06 -0500

    michael isn't that garbage ;-;

--- src/com/MRS/NeckbeardEngine/Enemies/Starburt.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Starburt.java
@@ -1,11 +1,16 @@
 package com.MRS.NeckbeardEngine.Enemies;
 
 import com.MRS.NeckbeardEngine.*;
+import com.MRS.NeckbeardEngine.Projectiles.*;
 
 public class Starburt extends Enemy {
   
+  public static int DEFAULT_HITBOX_WIDTH = 10;
+  public static int DEFAULT_HITBOX_HEIGHT = 10;
+  
   public Starburt (State state, int x, int y, double xVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
     super (state, x, y, xVelocity, 0, projectileType, heldPowerUp, timeLine, canShoot);
+    hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
     health = 2;
   }
   
