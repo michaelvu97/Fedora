f796aa9fe9abcf2ae28cafee3dcfecef43f6725c
TheImportantOne <safster4@gmail.com>
2014-11-11 21:29:00 -0500

    Fixing Dumb Git

--- src/com/MRS/NeckbeardEngine/Enemies/Shifter.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Shifter.java
@@ -8,14 +8,11 @@ import com.MRS.NeckbeardEngine.State;
 
 public class Shifter extends Enemy {
 	
-	public static int DEFAULT_HITBOX_WIDTH = 15;
+    public static int DEFAULT_HITBOX_WIDTH = 15;
     public static int DEFAULT_HITBOX_HEIGHT = 15;
-     
-     public static int DEFAULT_HITBOX_WIDTH = 10;
-     public static int DEFAULT_HITBOX_HEIGHT = 10;
+    
      public Shifter (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
-          hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
           health = 1;
           hitBox = new HitBox (x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
      }
