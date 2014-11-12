1f9716de06574b180dfe24d32a2d403b76016bfd
TheImportantOne <safster4@gmail.com>
2014-11-11 21:30:52 -0500

    Fixing Dumb Git

--- src/com/MRS/NeckbeardEngine/Enemies/Thug.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Thug.java
@@ -1,23 +1,5 @@
 package com.MRS.NeckbeardEngine.Enemies;
 
-<<<<<<< HEAD
-import com.MRS.NeckbeardEngine.Enemy;
-import com.MRS.NeckbeardEngine.HitBox;
-import com.MRS.NeckbeardEngine.PowerUpPickup;
-import com.MRS.NeckbeardEngine.Projectile;
-import com.MRS.NeckbeardEngine.State;
-
-public class Thug extends Enemy {
-  
-	public static int DEFAULT_HITBOX_WIDTH = 5;
-    public static int DEFAULT_HITBOX_HEIGHT = 5;
-    
-   public Thug (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup powerUpPickup, long timeLine, boolean canShoot) {
-        super(state, x, y, xVelocity, yVelocity, projectileType, powerUpPickup, timeLine, canShoot);
-        health = 2;
-        hitBox = new HitBox (x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
-        
-=======
 import com.MRS.NeckbeardEngine.*;
 import com.MRS.NeckbeardEngine.Projectiles.*;
 
@@ -29,7 +11,6 @@ public class Thug extends Enemy {
    public Thug (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup powerUpPickup, long timeLine, boolean canShoot) {
         super(state, x, y, xVelocity, yVelocity, projectileType, powerUpPickup, timeLine, canShoot);
         hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
->>>>>>> origin/master
   }
   
   @Override
