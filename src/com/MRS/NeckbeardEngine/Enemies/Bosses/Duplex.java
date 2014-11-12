be6383f7f55f5cda30dfdf6313f18e3fa75962b3
PeteDophile <PeteDophile@users.noreply.github.com>
2014-11-12 17:28:53 -0500

    Create Duplex.java

---
+++ src/com/MRS/NeckbeardEngine/Enemies/Bosses/Duplex.java
@@ -0,0 +1,33 @@
+package com.MRS.NeckbeardEngine.Enemies.Bosses;
+
+import com.MRS.NeckbeardEngine.HitBox;
+import com.MRS.NeckbeardEngine.Projectile;
+import com.MRS.NeckbeardEngine.State;
+
+public class Duplex {
+  private State state;
+  private int health;
+  private int x;
+  private int y;
+  private double xVelocity;
+  private double yVelocity;
+  private Projectile projectileType;
+  private int shiftTimer; //not sure if we should use this or just manipulate timeLine
+  private long timeLine;
+  private boolean canShoot;
+  
+  public static int DEFAULT_HITBOX_WIDTH = 10;
+  public static int DEFAULT_HITBOX_LENGTH = 10;
+  public Duplex () {
+    state = BOTH; //I don't know how enum works
+    health = 100;
+    x = 800; //placeholder
+    y = 800; //placeholder
+    xVelocity = 1;
+    yVelocity = 1;
+    projectileType = placeholder;
+    shiftTimer = 5;
+    timeLine = 0; //placeholder
+    canShoot = true;
+  }
+}
