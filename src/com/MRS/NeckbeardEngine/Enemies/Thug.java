ff985907b64202d046c6002551d7e34222311b45
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 19:46:38 -0500

    Imports and added hitbox constants

--- src/com/MRS/NeckbeardEngine/Enemies/Thug.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Thug.java
@@ -1,11 +1,16 @@
 package com.MRS.NeckbeardEngine.Enemies;
 
 import com.MRS.NeckbeardEngine.*;
+import com.MRS.NeckbeardEngine.Projectiles.*;
 
 public class Thug extends Enemy {
   
+  public static int DEFAULT_HITBOX_WIDTH = 10;
+  public static int DEFAULT_HITBOX_HEIGHT = 10;
+  
    public Thug (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup powerUpPickup, long timeLine, boolean canShoot) {
         super(state, x, y, xVelocity, yVelocity, projectileType, powerUpPickup, timeLine, canShoot);
+        hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
   }
   
   @Override
