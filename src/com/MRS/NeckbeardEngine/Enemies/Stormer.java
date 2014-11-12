1c7f7c8c1dc9279e145b037c7348b896d648ef2a
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 20:00:24 -0500

    Imports and added hitbox constants

--- src/com/MRS/NeckbeardEngine/Enemies/Stormer.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Stormer.java
@@ -4,8 +4,12 @@ import com.MRS.NeckbeardEngine.*;
 
 public class Stormer extends Enemy {
      
+     public static int DEFAULT_HITBOX_WIDTH = 10;
+     public static int DEFAULT_HITBOX_HEIGHT = 10;
+     
      public Stormer (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
+          hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
           health = 1;
      }
      @Override
