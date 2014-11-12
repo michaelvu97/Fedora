3dfd061574c679702958ec1e6cc59ee0e415d89c
Michael Vu <michael.vu.97@outlook.com>
2014-11-11 19:42:05 -0500

    Imports and added hitbox constants

--- src/com/MRS/NeckbeardEngine/Enemies/Vortex.java
+++ src/com/MRS/NeckbeardEngine/Enemies/Vortex.java
@@ -1,11 +1,16 @@
 package com.MRS.NeckbeardEngine.Enemies;
 
 import com.MRS.NeckbeardEngine.*;
+import com.MRS.NeckbeardEngine.*;
 
 public class Vortex extends Enemy {
      
+     public static int DEFAULT_HITBOX_WIDTH = 10;
+     public static int DEFAULT_HITBOX_HEIGHT = 10;
+     
      public Vortex (State state, int x, int y, double xVelocity, double yVelocity, Projectile projectileType, PowerUpPickup heldPowerUp, long timeLine, boolean canShoot) {
           super(state, x, y, xVelocity, yVelocity, projectileType, heldPowerUp, timeLine, canShoot);
+          hitBox = new HitBox(x, y, DEFAULT_HITBOX_WIDTH, DEFAULT_HITBOX_HEIGHT);
           health = 4;
      }
      @Override
