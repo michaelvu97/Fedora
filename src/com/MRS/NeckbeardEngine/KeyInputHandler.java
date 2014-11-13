6e088ca288162c84f7be10f92431b9b12b36c1a4
Michael Vu <michael.vu.97@outlook.com>
2014-11-12 19:56:17 -0500

    Update KeyInputHandler.java

--- src/com/MRS/NeckbeardEngine/KeyInputHandler.java
+++ src/com/MRS/NeckbeardEngine/KeyInputHandler.java
@@ -10,37 +10,26 @@
 package com.MRS.NeckbeardEngine;
 
 import java.awt.event.KeyEvent;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.io.*;
 
 public class KeyInputHandler {
      
      public boolean up = false, down = false, left = false, right = false, shoot = false, switchState = false, bomb = false;
      
-     public final String path = "Data\\KeyBindings.txt";
+     public final String path = "KeyBindings.txt";
      
      /*
       * Default Keybindings
       */
      private int keyUp = KeyEvent.VK_UP, keyDown = KeyEvent.VK_DOWN, keyLeft = KeyEvent.VK_LEFT, keyRight = KeyEvent.VK_RIGHT, keyShoot = KeyEvent.VK_Z, keySwitchState = KeyEvent.VK_SPACE,keyBomb = KeyEvent.VK_X; 
-    /* 
+    
      public KeyInputHandler() {
-          //Read from file
-          try {
-               String[] keyBindings = openFile();
-               keyUp = Integer.parseInt(keyBindings[0]);
-               keyDown = Integer.parseInt(keyBindings[1]);
-               keyLeft = Integer.parseInt(keyBindings[2]);  //DO WE NEED THIS??????
-               keyRight = Integer.parseInt(keyBindings[3]);
-               keyShoot = Integer.parseInt(keyBindings[4]);
-               keySwitchState = Integer.parseInt(keyBindings[5]);
-               keyBomb = Integer.parseInt(keyBindings[6]);
-          } catch (Exception e) {
-               e.printStackTrace();
-          }
+          File f = new File(path);
+          ArrayList<KeyValuePair> pairs = DataHandler.parseFile(f);
+          //add accessing keys
      }
-    */ 
+     
      public void sendKeyPressed (int keyPressed) {
           //KeyEvent comparasin
           if (keyPressed == keyUp) {
@@ -89,23 +78,5 @@ public class KeyInputHandler {
           if(keyReleased == keyBomb) {
         	  bomb = false;
           }
-     }
-     
-     public String[] openFile() throws IOException {
-          //Setup readers for file
-          FileReader f = new FileReader(path);
-          BufferedReader textReader = new BufferedReader(f);
-          
-          int lines = 7; //change according to file;
-          String[] textData = new String[lines];
-          
-          //set array data line by line
-          for (int i = 0; i < lines; i++) {
-               textData[i] = textReader.readLine();
-          }
-          
-          //close reader and return data
-          textReader.close();
-          return textData;
      }
 }
