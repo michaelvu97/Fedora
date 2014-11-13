2f9e5fd5023d2be1b09ea0317018e8bee1186ee6
TheImportantOne <safster4@gmail.com>
2014-11-13 13:10:06 -0500

    Added Images

--- src/com/MRS/NeckbeardEngine/Projectiles/Shot.java
+++ src/com/MRS/NeckbeardEngine/Projectiles/Shot.java
@@ -2,10 +2,17 @@ package com.MRS.NeckbeardEngine.Projectiles;
 
 import com.MRS.NeckbeardEngine.*;
 import java.awt.*;
+import java.awt.Image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
 
 public class Shot extends Projectile {
+  BufferedImage img = null;
   public Shot (State state, int x, int y, double xVelocity, double yVelocity, String imgPath, HitBox hitBox, double duration) {
     super(state, x, y, xVelocity, yVelocity, imgPath, hitBox, duration);
+    try {
+      img = ImageIO.read(new File(imgPath));
+    } catch(IOException ioe){}
   }
   
   public void move() {
@@ -15,6 +22,6 @@ public class Shot extends Projectile {
   
   @Override
   public void paint(Graphics2D g2d) {
-       
+       g2d.draw(img,x,y,null);
   }
 }
