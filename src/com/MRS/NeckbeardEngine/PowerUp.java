6df13ce6e0601fd4dc8957b4aa0be61313fbb15f
Michael Vu <michael.vu.97@outlook.com>
2014-12-05 13:40:21 -0500

    Changed to enumeration

--- src/com/MRS/NeckbeardEngine/PowerUp.java
+++ src/com/MRS/NeckbeardEngine/PowerUp.java
@@ -1,5 +1,21 @@
 package com.MRS.NeckbeardEngine;
 
-public abstract class PowerUp {
-     //null
+public enum PowerUp {
+  FAST_SHOT(0, true),
+  RAPID_FIRE(1, true),
+  PIERCING_SHOT(2, true);
+  
+  private int powerUp;
+  private boolean offensive;
+  private PowerUp (int powerUp, boolean offensive) {
+    this.powerUp = powerUp;
+    this.offensive = offensive;
+  }
+  
+  public int getPowerUp() {
+    return powerUp;
+  }
+  public boolean getOffensive() {
+    return offensive;
+  }
 }
