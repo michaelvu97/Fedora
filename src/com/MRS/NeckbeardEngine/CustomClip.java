c0cabe1e9b082ccb4d49f85b9c54e4e531c5d43a
Michael Vu <michael.vu.97@outlook.com>
2014-12-09 12:27:01 -0500

    Documentation

--- src/com/MRS/NeckbeardEngine/CustomClip.java
+++ src/com/MRS/NeckbeardEngine/CustomClip.java
@@ -2,8 +2,14 @@ package com.MRS.NeckbeardEngine;
 
 import javax.sound.sampled.Clip;
 
-public class CustomClip{
+public class CustomClip {
+  /*
+   * Stores an identifier and an 
+   * audio clip, so that they are
+   * paired together.
+   */
   String id;
   Clip c;
+  
   public CustomClip() {}
 }
