2d23ec581b44b887799d0059d741d7ee1abff7fb
PeteDophile <PeteDophile@users.noreply.github.com>
2014-12-12 12:09:49 -0500

    Added Extra Life, Speed Boost, Scatter Shot

--- src/com/MRS/NeckbeardEngine/PowerUp.java
+++ src/com/MRS/NeckbeardEngine/PowerUp.java
@@ -3,8 +3,10 @@ package com.MRS.NeckbeardEngine;
 public enum PowerUp {
   FAST_SHOT(0, true),
   RAPID_FIRE(1, true),
-  PIERCING_SHOT(2, true),
-  BOMB(3,true);
+  SCATTER_SHOT(2, true),
+  BOMB(3,true),
+  EXTRA_SHIP(0, false),
+  SPEED_BOOST(1, false);
   
   private int powerUp;
   private boolean offensive;
