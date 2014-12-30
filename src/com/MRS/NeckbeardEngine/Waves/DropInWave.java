deee1c20f943c3af69cd9539c0e7177023ac8039
michaelvu97 <michael.vu.97@outlook.com>
2014-12-30 18:29:18 -0500

    Starting point for waves

---
+++ src/com/MRS/NeckbeardEngine/Waves/DropInWave.java
@@ -0,0 +1,11 @@
+package com.MRS.NeckbeardEngine.Waves;
+
+import com.MRS.NeckbeardEngine.*;
+import com.MRS.NeckbeardEngine.Enemies.*;
+import java.util.ArrayList;
+
+public class DropInWave extends Wave {
+  public DropInWave () {
+    enemies = new ArrayList<Enemy>();
+  }
+}
