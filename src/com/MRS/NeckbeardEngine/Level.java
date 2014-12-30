2e07505db0739057f9b38c0d8335c7f2ad7eb628
michaelvu97 <michael.vu.97@outlook.com>
2014-12-30 18:38:31 -0500

    Very basic wave created

--- src/com/MRS/NeckbeardEngine/Level.java
+++ src/com/MRS/NeckbeardEngine/Level.java
@@ -2,6 +2,9 @@ package com.MRS.NeckbeardEngine;
 
 import com.MRS.NeckbeardEngine.Enemies.*;
 import com.MRS.NeckbeardEngine.Projectiles.*;
+import com.MRS.NeckbeardEngine.Waves.*;
+import java.util.ArrayList;
+
 public class Level {
   
   Game g;
@@ -17,7 +20,11 @@ public class Level {
     
     switch(frames) {
       case 180:
-        g.enemies.add(new Elite(State.BLUE, 300, -100, 0, 3, "Shot", null, 0, target));
+        ArrayList<Enemy> e1 = new DropInWave().unpack();
+        for (int i = 0; i < e1.size(); i++) {
+          g.enemies.add(e1.get(i));
+        }
+//        g.enemies.add(new Elite(State.BLUE, 300, -100, 0, 3, "Shot", null, 0, target));
 //        g.enemies.add(new Shifter(State.BLUE, -72, 100, 3, 0, "Shot", null, 0, "stay", target, 0));
 //        g.enemies.add(new Iris(State.BLUE, 100, -128, 3, "Laser", null, 0));
 //        g.enemies.add(new Iris(State.BLUE, 500, -128, 3, "Laser", null, 0));
@@ -28,49 +35,49 @@ public class Level {
 //        g.enemies.add(new Mook(State.BLUE, 750, -50, -3, 4, "Shot", null, 0, "form3", Main.WIDTH/4));
 //        g.enemies.add(new Mook(State.RED, 810, -110, -3, 4, "Shot", null, 0, "form3", Main.WIDTH/4));
 //        
-        break;
-      case 750:
-        g.enemies.add(new Mook(State.BLUE, 720, 800, -4, -4, "Shot", null, 0, "form2", Main.WIDTH/2));
-        g.enemies.add(new Mook(State.RED, 780, 860, -4, -4, "Shot", null, 0, "form2", Main.WIDTH/2));
-      case 840:
-        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 900:
-        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 1200:
-        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        g.enemies.add(new Mook(State.BLUE, 0, 0, 1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 1800:
-        g.enemies.add(new Mook(State.RED, 100, -60, 1, 15, "Shot", null, 0, "stay", Main.WIDTH/2));
-        break;
-      case 2400:
-        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 3000:
-        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        g.enemies.add(new Mook(State.RED, 0, 70, 1, 15, "Shot", null, 0, "stay", Main.WIDTH/2));
-        break;
-      case 3600:
-        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        g.enemies.add(new Mook(State.RED, 0, 70, 1, 15, "Shot", null, 0, "stay", Main.WIDTH/2));
-        break;
-      case 3660:
-        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 3720:
-        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 3780:
-        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 3840:
-        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
-      case 3900:
-        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
-        break;
+//        break;
+//      case 750:
+//        g.enemies.add(new Mook(State.BLUE, 720, 800, -4, -4, "Shot", null, 0, "form2", Main.WIDTH/2));
+//        g.enemies.add(new Mook(State.RED, 780, 860, -4, -4, "Shot", null, 0, "form2", Main.WIDTH/2));
+//      case 840:
+//        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 900:
+//        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 1200:
+//        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        g.enemies.add(new Mook(State.BLUE, 0, 0, 1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 1800:
+//        g.enemies.add(new Mook(State.RED, 100, -60, 1, 15, "Shot", null, 0, "stay", Main.WIDTH/2));
+//        break;
+//      case 2400:
+//        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 3000:
+//        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        g.enemies.add(new Mook(State.RED, 0, 70, 1, 15, "Shot", null, 0, "stay", Main.WIDTH/2));
+//        break;
+//      case 3600:
+//        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        g.enemies.add(new Mook(State.RED, 0, 70, 1, 15, "Shot", null, 0, "stay", Main.WIDTH/2));
+//        break;
+//      case 3660:
+//        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 3720:
+//        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 3780:
+//        g.enemies.add(new Mook(State.BLUE, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 3840:
+//        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
+//      case 3900:
+//        g.enemies.add(new Mook(State.RED, 600, 0, -1, 10, "Shot", null, 0, "leave", Main.WIDTH/2));
+//        break;
       default:
         
     }
--- src/com/MRS/NeckbeardEngine/Waves/DropInWave.java
+++ src/com/MRS/NeckbeardEngine/Waves/DropInWave.java
@@ -6,6 +6,16 @@ import java.util.ArrayList;
 
 public class DropInWave extends Wave {
   public DropInWave () {
-    enemies = new ArrayList<Enemy>();
+    super();
+    
+    //probability of states
+    int chance = (int) (Math.random() * 2);
+    State waveType = State.RED;
+    if (chance == 0) {
+      waveType = State.RED;
+    } else {
+      waveType = State.BLUE;
+    }
+    enemies.add(new Mook(waveType, -30, -50, 3, 4, "Shot", null, 0, "stay", Main.WIDTH/4));
   }
 }
