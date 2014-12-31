f5073b5014d8cb5bdf46f1daed67d7b9ed14fdf8
michaelvu97 <michael.vu.97@outlook.com>
2014-12-31 15:20:56 -0500

    Spawnevent + wave documentation

--- src/com/MRS/NeckbeardEngine/SpawnEvent.java
+++ src/com/MRS/NeckbeardEngine/SpawnEvent.java
@@ -1,6 +1,22 @@
 package com.MRS.NeckbeardEngine;
 
 public class SpawnEvent {
+  /* README
+   * SpawnEvents are simply a pairing of an enemy
+   * and the frame at which it should spawn.
+   * 
+   * SpawnEvents are opened in levels and are
+   * unpacked from a wave.
+   * 
+   * The purpose of a 
+   * SpawnEvent is to be able to offset the 
+   * Enemies spawned in a wave, without having to
+   * modify the level too greatly.
+   * 
+   * See the Waves package for the different waves
+   * and implementation of SpawnEvents
+   */
+  
   private int spawnFrame;
   private Enemy spawnable;
   
