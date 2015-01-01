03d230fa769db85398fa1fc3d2e08cb88f14bd13
michaelvu97 <michael.vu.97@outlook.com>
2015-01-01 18:01:38 -0500

    Starburt wave available

---
+++ src/com/MRS/NeckbeardEngine/Waves/StarburtWave.java
@@ -0,0 +1,32 @@
+package com.MRS.NeckbeardEngine.Waves;
+
+import com.MRS.NeckbeardEngine.Enemies.*;
+import com.MRS.NeckbeardEngine.*;
+import java.util.ArrayList;
+
+public class StarburtWave {
+  /*
+   * For help with waves, refer
+   * to "How To Wave.txt" in 
+   * the waves package
+   */
+  
+  //uninstatiatable
+  private StarburtWave () {}
+  
+  /*
+   * A simple Starburt wave: spawns a starburt at a given xLocation, offscreen, and with a given state
+   */
+  
+  public static ArrayList<SpawnEvent> unpack (State targetState, int currentFrame, int xLocation) {
+    ArrayList<SpawnEvent> events = new ArrayList<SpawnEvent>();
+    
+    events.add (new SpawnEvent (
+                                     new Starburt(targetState, -200, 100, 5, 10, "StarburtShot", null, 0)
+                                     , currentFrame
+                                     ));
+    
+    return events;
+  }
+  
+}
